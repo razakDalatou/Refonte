44554ab27b57cf49bcd592884f3125bea6b8e3a7
dalatouar <abdelrazak.dalatou@gmail.com>
2025-06-14 15:38:33 +0200

    #"first commit"

---
+++ src/Controller/PageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PageController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', [
+    
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', [
+        
+        ]);
+    }
+}
